08d961b692cc2451c7a354cc4e0757505577ee7b
TexHaPb <texhapb@bk.ru>
2021-01-25 12:08:59 +0300

    Added welcome page

--- Quiz Test/WelcomeViewController.swift
+++ Quiz Test/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  Quiz Test
 //
 //  Created by Максим Иванов on 04.01.2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
