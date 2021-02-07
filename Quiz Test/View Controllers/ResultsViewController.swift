3aeb63115d78e78e90c43b1a83334db4f6d321a8
TexHaPb <texhapb@bk.ru>
2021-02-07 12:43:13 +0300

    Added result screen

---
+++ Quiz Test/View Controllers/ResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsViewController.swift
+//  Quiz Test
+//
+//  Created by Максим Иванов on 07.02.2021.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+   
+
+}
--- Quiz Test/View Controllers/WelcomeViewController.swift
+++ Quiz Test/View Controllers/WelcomeViewController.swift
@@ -14,6 +14,9 @@ class WelcomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
 
 }
 
